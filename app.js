0c3aabc0540250b0e268689365142aabb2759b17
Juan Lagos <jramonlagos.jrlp@gmail.com>
2025-03-18 17:51:35 -0600

    Se creó array para almacenar los nombres de amigos y el método agregarAmigo para validar y almacenarlos.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar nombres de amigos
+let nombres = [];
+
+
+function agregarAmigo() {
+    let input = document.getElementById('amigo').value;
+
+    if (input.trim() == '' || input.trim().match('\^\\d+$')) {
+        alert('¡Por favor, ingrese un nombre válido!');
+    } else {
+        if(nombres.includes(input)) {
+            alert('¡El nombre ya se encuentra registrado!');
+        } else {
+            nombres.push(input);
+        }
+    }
+}
