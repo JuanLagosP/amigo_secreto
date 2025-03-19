d7906cd6582ac257a2412d28607484825c3cde28
Juan Lagos <jramonlagos.jrlp@gmail.com>
2025-03-18 18:40:34 -0600

    Se añadió método para limpiar lista al sortear el amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar nombres de amigos
+let amigos = [];
+
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+
+    if (nombre.trim() == '' || nombre.trim().match('\^\\d+$')) {
+        alert('¡Por favor, ingrese un nombre válido!');
+    } else {
+        if(amigos.includes(nombre)) {
+            alert('¡El nombre ya se encuentra registrado!');
+        } else {
+            amigos.push(nombre);
+            actualizarListaAmigos();
+        }
+    }
+
+    limpiarInput();
+}
+
+function limpiarInput() {
+    document.getElementById('amigo').value = '';
+}
+
+function limpiarLista() {
+    document.getElementById('listaAmigos').innerHTML = '';
+}
+
+function actualizarListaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    
+    limpiarLista();
+    
+    amigos.forEach(nombre => {
+        lista.innerHTML += `<li>${nombre}</li>`;
+    });
+}
+
+function sortearAmigo() {
+    let longitudLista = amigos.length;
+
+    if (longitudLista == 0) {
+        alert('¡No se ha añadido ningún amigo!')
+        return;
+    } else {
+        let indiceAleatorio = Math.floor(Math.random() * longitudLista);
+        let amigoSecreto = amigos[indiceAleatorio];
+
+        limpiarLista();
+        document.getElementById('resultado').innerHTML = `El amigo secreto sorteado es: ${amigoSecreto}`;
+    }
+}
+
